53602505b8e35b47e5b6e8dc64c9f2b34f5bbf99
Andrew Kopanev <andrew@moqod.com>
2016-06-20 18:08:34 +0300

    added bot

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// imports
 var restify = require('restify');
+var builder = require('botbuilder');
+
+// constants
 var port = process.env.PORT || 3000;
 
 function respond(req, res, next) {
+  res.contentType = "text/plain";
   res.send('Your bot response :)');
   next();
 }
@@ -19,3 +24,12 @@ server.post('/request', handleRequestMessage);
 server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
+
+// bot creation
+var bot = new builder.BotConnectorBot({ appId: 'ProivderBot', appSecret: '27da870722c84fa5b7f33bb1e8f3bbd8' });
+bot.add('/', function (session) {
+    session.send('Provider bot in operation :-)');
+});
+
+// Setup Restify Server
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
