6df3e0aba1cd629bb50c0eec9b4d232276d42918
moqod <moqod@albina.local>
2016-06-30 15:02:57 +0400

    few changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // imports
 var restify = require('restify');
 var builder = require('botbuilder');
+var mongoose = require('mongoose');
 
 // constants
 var port = process.env.PORT || 3000;
@@ -29,7 +30,30 @@ server.listen(port, function() {
 var bot = new builder.BotConnectorBot({ appId: 'ProivderBot', appSecret: '27da870722c84fa5b7f33bb1e8f3bbd8' });
 bot.add('/', function (session) {
     session.send('Provider bot in operation :-)');
+    Providers.findOne({ channelId: session.from.channelId }, function(err, exmpl1) {
+      if (err) return console.error(err);
+      if (exmpl1 == null)
+      {
+        var item = new Providers(session.from);
+        item.save();
+      };
+    });
 });
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function() {
+  console.log("connection DB ok");
+});
+var providersSchemaFrom = new mongoose.Schema({
+  "name": "string",
+  "channelId": "string",
+  "address": "string",
+  "id": "string",
+  "isBot": true
+  });
+var Providers = mongoose.model('Providers', providersSchemaFrom);
 
 // Setup Restify Server
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+mongoose.connect(process.env.MONGO_URI||'mongodb://sazha:sazha123@ds035485.mlab.com:35485/telegrambot');
