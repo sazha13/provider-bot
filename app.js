1592fc395662e4140a7b31076885883acc6f4039
sasa <sasa@sasa-desktop>
2016-07-19 15:51:01 +0400

    few change

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ server.listen(port, function() {
 var bot = new builder.BotConnectorBot({ appId: 'ProivderBot', appSecret: '27da870722c84fa5b7f33bb1e8f3bbd8' });
 bot.add('/', function (session) {
     //session.send('Provider bot in operation :-)');
+	session.message.BotPerUserInConversationData = null;
     var from1 = session.message;
     var recvedMsg = session.message;
     ServerMsg = 'HERE';
